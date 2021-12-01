2c430a87b5bfee4f1658c81a5632bc91c9c1c328
Eric <yantisdev@gmail.com>
2021-12-01 02:32:59 -0700

    challenge 1 start

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+// challenge 1
+//Test Data Set 1
+let markWtKg = 78;
+let markHtM = 1.69;
+let johnWtKg = 92;
+let johnHtM = 1.95;
+
+//Test Data Set 2
+let mark2WtKg = 95;
+let mark2HtM = 1.88;
+let john2WtKg = 85;
+let john2HtM = 1.76;
+
+let markBMI1 = markWtKg / markHtM ** 2;
+let johnBMI1 = johnWtKg / johnHtM ** 2;
+
+console.log(markBMI1, johnBMI1);
