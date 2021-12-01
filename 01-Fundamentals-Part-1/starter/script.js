322ef7b5369c35ffaf3bc358065a174700d28872
Eric <yantisdev@gmail.com>
2021-12-01 09:01:58 -0700

    TAKING DECISIONS: IF / ELSE STATEMENTS LECTURE START

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -28,6 +28,38 @@
 
 // let markHigherBMI2 = markBMI2 > johnBMI2;
 // console.log(markHigherBMI2);
-//Challenge 1 - What I Learned - I Learned I could have used less variables and still used 'const because  every-time you reload the webpage it clears the memory and stores all the variables all over again..
+// //Challenge 1 - What I Learned - I Learned I could have used less variables and still used 'const because  every-time you reload the webpage it clears the memory and stores all the variables all over again..
 
-//Strings and Template Literals Lecture and Notes
+// //Strings and Template Literals Lecture and Notes
+
+// const firstName = "Eric";
+// const job = "Developer";
+// const birthYear = 1980;
+// const currentYear = 2021;
+
+// // This is before JS6 Template Literals...it is annoying and wordy to code.
+// // const eric =
+// //     "I'm " +
+// //     firstName +
+// //     ", a " +
+// //     (currentYear - birthYear) +
+// //     "year old " +
+// //     job +
+// //     "!";
+
+// // console.log(eric);
+
+// //Here is the same thing but with Template Literals (note = you have to use back ticks not quotes single or double to start these.)
+
+// const ericNew = `I'm ${firstName}, a ${
+//     currentYear - birthYear
+// } year old ${job}!`;
+// console.log(ericNew);
+
+// //Template Literal Back Tick (`) also allow multiple line strings (useful later)
+// console.log(`String
+// multiple
+// lines`);
+
+//END STRINGS AND TEMPLATE LITERALS LECTURE
+//START TAKING DECISIONS: IF / ELSE STATEMENTS
