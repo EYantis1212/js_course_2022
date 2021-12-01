18b577d88c743690ed837630124f0420522938a0
Eric <yantisdev@gmail.com>
2021-12-01 02:38:37 -0700

    Challenge 1 Solution 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -11,7 +11,20 @@ let mark2HtM = 1.88;
 let john2WtKg = 85;
 let john2HtM = 1.76;
 
+//BMI Data Set 1
 let markBMI1 = markWtKg / markHtM ** 2;
 let johnBMI1 = johnWtKg / johnHtM ** 2;
 
 console.log(markBMI1, johnBMI1);
+
+let markHigherBMI = markBMI1 > johnBMI1;
+console.log(markHigherBMI);
+
+//BMI Data Set 2
+let markBMI2 = mark2WtKg / mark2HtM ** 2;
+let johnBMI2 = john2WtKg / john2HtM ** 2;
+
+console.log(markBMI2, johnBMI2);
+
+let markHigherBMI2 = markBMI2 > johnBMI2;
+console.log(markHigherBMI2);
