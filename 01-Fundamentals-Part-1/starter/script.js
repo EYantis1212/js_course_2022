82b8c13669b3e3d863400cd27f78584b286936c3
Eric <yantisdev@gmail.com>
2021-12-01 02:53:18 -0700

    Challenge 1Complete and What LI Learned Written

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,30 +1,31 @@
-// challenge 1
+// Challenge 1 My Solution
 //Test Data Set 1
-let markWtKg = 78;
-let markHtM = 1.69;
-let johnWtKg = 92;
-let johnHtM = 1.95;
+// let markWtKg = 78;
+// let markHtM = 1.69;
+// let johnWtKg = 92;
+// let johnHtM = 1.95;
 
-//Test Data Set 2
-let mark2WtKg = 95;
-let mark2HtM = 1.88;
-let john2WtKg = 85;
-let john2HtM = 1.76;
+// //Test Data Set 2
+// let mark2WtKg = 95;
+// let mark2HtM = 1.88;
+// let john2WtKg = 85;
+// let john2HtM = 1.76;
 
-//BMI Data Set 1
-let markBMI1 = markWtKg / markHtM ** 2;
-let johnBMI1 = johnWtKg / johnHtM ** 2;
+// //BMI Data Set 1
+// let markBMI1 = markWtKg / markHtM ** 2;
+// let johnBMI1 = johnWtKg / johnHtM ** 2;
 
-console.log(markBMI1, johnBMI1);
+// console.log(markBMI1, johnBMI1);
 
-let markHigherBMI = markBMI1 > johnBMI1;
-console.log(markHigherBMI);
+// let markHigherBMI = markBMI1 > johnBMI1;
+// console.log(markHigherBMI);
 
-//BMI Data Set 2
-let markBMI2 = mark2WtKg / mark2HtM ** 2;
-let johnBMI2 = john2WtKg / john2HtM ** 2;
+// //BMI Data Set 2
+// let markBMI2 = mark2WtKg / mark2HtM ** 2;
+// let johnBMI2 = john2WtKg / john2HtM ** 2;
 
-console.log(markBMI2, johnBMI2);
+// console.log(markBMI2, johnBMI2);
 
-let markHigherBMI2 = markBMI2 > johnBMI2;
-console.log(markHigherBMI2);
+// let markHigherBMI2 = markBMI2 > johnBMI2;
+// console.log(markHigherBMI2);
+//Challenge 1 - What I Learned - I Learned I could have used less variables and still used 'const because  every-time you reload the webpage it clears the memory and stores all the variables all over again..
