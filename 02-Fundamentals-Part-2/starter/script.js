79859fcaaf09636890b3ee7eb44c1c02b0194c1f
Eric <yantisdev@gmail.com>
2021-12-02 02:57:37 -0700

    END FUNCTIONS LECTURE / START FUNCTION DECLARATIONS vs EXPRESSIONS LECTURE

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -3,3 +3,29 @@
 "use strict";
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //START FUNCTIONS LECTURE
+
+function logger() {
+    console.log("My name is Eric");
+}
+
+// calling / running / invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} Apples and ${oranges} Oranges`;
+    return juice;
+}
+
+//AppleJuice!
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+//AppleOrange Juice
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+// END FUNCTIONS LECTURE
+///////////////////////////////////////////////////////////////////////////////////////////////
+//START FUNCTION DECLARATIONS vs EXPRESSIONS LECTURE
