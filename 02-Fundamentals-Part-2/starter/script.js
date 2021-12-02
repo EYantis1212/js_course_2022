8f4eca0ae16bff1853fb823fe596dc1debcc1a8b
Eric <yantisdev@gmail.com>
2021-12-02 04:50:45 -0700

    END ARROW FUNCTIONS LECTURE /START FUNCTIONS CALLING OTHER FUNCTIONS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -36,19 +36,42 @@
 //     return age;
 // }
 //Simplified
-function calcAge1(birthYear) {
-    return 2037 - birthYear;
-}
-const age1 = calcAge1(1980);
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1980);
 
-//FUNCTION EXPRESSION
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-};
+// //FUNCTION EXPRESSION
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// };
 
-const age2 = calcAge2(1983);
-console.log(age1, age2);
+// const age2 = calcAge2(1983);
+// console.log(age1, age2);
 
 // END FUNCTION DECLARATIONS vs EXPRESSIONS LECTURE
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //START ARROW FUNCTIONS LECTURE
+
+//FUNCTION EXPRESSION
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// };
+
+//ARROW FUNCTION (We do not understand this yet...but arrow functions do not get "this" keyword...we will learn this later. )
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// const age3 = calcAge3(1980);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2021 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} has ${retirement} years until retirement!`;
+// };
+// console.log(yearsUntilRetirement(1980, "Eric"));
+// console.log(yearsUntilRetirement(1983, "Rebecca"));
+
+// END ARROW FUNCTIONS LECTURE
+///////////////////////////////////////////////////////////////////////////////////////////////
+// START FUNCTIONS CALLING OTHER FUNCTIONS
