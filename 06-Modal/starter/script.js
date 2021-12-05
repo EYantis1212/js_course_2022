5928642a98891609cda20b7bf1f10d3c1c128d9c
Yantis <yantis1212@outlook.com>
2021-12-05 08:50:22 -0700

    END PROJECT #2

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,2 +1,34 @@
 'use strict';
 // START PROJECT #2 //
+
+// Selectors
+const targetDiv = document.querySelector('.hidden');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+// Open/Close Function
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Listeners
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+/* Using the class .hidden lets you manipulate multiple properties in one shot instead of just changing something like display = 'block' which is only one property.
+ */
