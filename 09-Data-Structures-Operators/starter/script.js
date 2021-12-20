d2ebe470ddd56c90d79d0618db4eba52a6228826
Yantis <yantis1212@outlook.com>
2021-12-20 15:48:23 -0700

    END LOOPING OBJECTS / START CODING CHALLENGE 2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -565,59 +565,59 @@ GOOD LUCK �
 // }
 // fizzBuzz(31);
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-for (const item of menu) console.log(item);
-// Here item is a variable you can name anything...the for loop iterates over the array
-// It then pulls the elements of the arrays into the variable
+// for (const item of menu) console.log(item);
+// // Here item is a variable you can name anything...the for loop iterates over the array
+// // It then pulls the elements of the arrays into the variable
 
-// If you want the index and the element you have to do this.
-for (const item of menu.entries()) {
-    console.log(item);
-}
+// // If you want the index and the element you have to do this.
+// for (const item of menu.entries()) {
+//     console.log(item);
+// }
 
-// With Destructuring
-for (const [i, el] of menu.entries()) {
-    console.log(i, el);
-}
+// // With Destructuring
+// for (const [i, el] of menu.entries()) {
+//     console.log(i, el);
+// }
 
 // Enhanced Object Literals
 
 // Computing
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-const openingHrs = {
-    [weekdays[0]]: {
-        open: 12,
-        close: 23,
-    },
-    [weekdays[1 + 2]]: {
-        // Thu
-        open: 11,
-        close: 22,
-    },
-    [`${weekdays[5]}urday`]: {
-        //Saturday
-        open: 10,
-        close: 24,
-    },
-};
-console.log(openingHrs); // Mon: open:12 close:23
+// const openingHrs = {
+//     [weekdays[0]]: {
+//         open: 12,
+//         close: 23,
+//     },
+//     [weekdays[1 + 2]]: {
+//         // Thu
+//         open: 11,
+//         close: 22,
+//     },
+//     [`${weekdays[5]}urday`]: {
+//         //Saturday
+//         open: 10,
+//         close: 24,
+//     },
+// };
+// console.log(openingHrs); // Mon: open:12 close:23
 
 // END ENHANCED OBJECT LITERALS
 ////////////////////////////////////////////////
 // START OPTIONAL CHAINING (?)
 
-// If you get info from an API or something you might not know what is in an object
-//Here we are trying to see the hours IF the restaurant is open on monday
-if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+// // If you get info from an API or something you might not know what is in an object
+// //Here we are trying to see the hours IF the restaurant is open on monday
+// if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
 
-// ENTER OPTIONAL CHAINING
+// // ENTER OPTIONAL CHAINING
 
-// Here...if Monday exists then get the opening hours.
-console.log(restaurant.openingHours.mon?.open); //undefined but no error
-//Now checking if opening hours even exists first
-console.log(restaurant.openingHours?.mon?.open);
+// // Here...if Monday exists then get the opening hours.
+// console.log(restaurant.openingHours.mon?.open); //undefined but no error
+// //Now checking if opening hours even exists first
+// console.log(restaurant.openingHours?.mon?.open);
 
 // More real world example
 const wkdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
@@ -630,31 +630,142 @@ const wkdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 // }
 // If we want to get rid of the undefined we can set a default value like:
 // We use a nullish coalescing operator (??) so that Saturdays opening hour of '0' returns even though it is falsy
-for (const day of wkdays) {
-    console.log(day);
-    const open = restaurant.openingHours[day]?.open ?? 'closed';
-    console.log(open);
+// for (const day of wkdays) {
+//     console.log(day);
+//     const open = restaurant.openingHours[day]?.open ?? 'closed';
+//     console.log(open);
+// }
+
+// // Methods
+// //Here we can check if a method exists before calling it
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist'); // Focaccia, Pasta
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist'); // Method does not exist
+
+// // Arrays
+// let currentYear = new Date().getFullYear();
+// console.log(currentYear);
+// const family = [
+//     { name: 'Eric', age: currentYear - 1980 },
+//     { name: 'Rebecca', age: currentYear - 1983 },
+//     { name: 'Joseph', age: currentYear - 2004 },
+//     { name: 'Edward', age: currentYear - 2007 },
+//     { name: 'Zachary', age: currentYear - 2009 },
+// ];
+// console.log(family);
+// console.log(family[0]?.name) ?? 'Array Empty'; //Eric
+// console.log(family[0]?.age) ?? 'Array Empty'; //41
+
+// // END OPTIONAL CHAINING (?)
+// ////////////////////////////////////////////////
+// // START LOOPING OBJECTS: OBJECT KEYS, VALUES, and ENTRIES
+
+// // Object Property Names/Keys
+
+// const properties = Object.keys(openingHrs);
+// console.log(properties); // Array with all keys from object (mon, thu, saturday)
+
+// // for (const day of Object.keys(openingHrs)) {
+// //     console.log(day); // Pulled all the keys from the object into a variable
+// // }
+// let openStr = `We are open on ${properties.length} days: `;
+// for (const day of properties) {
+//     openStr += `${day}, `;
+// }
+// console.log(openStr); // We are open on 3 days: mon, thu, saturday
+
+// // Object Property Values
+
+// const values = Object.values(openingHrs);
+// console.log(values); // open and close for all entries just no days because those are keys
+
+// // Object Entries (Both keys and values)
+// // const entries = Object.entries(openingHrs);
+// // console.log(entries);
+
+// // for (const [key, { open, close }] of entries) {
+// //     console.log(`On ${key} we open at ${open} and close at ${close}`);
+// // }
+// //On mon we open at 12 and close at 23
+// //On thu we open at 11 and close at 22
+// //On saturday we open at 10 and close at 24
+
+// const entries = Object.entries(family);
+// console.log(entries);
+
+// for (const [entry, { name, age }] of entries) {
+//     console.log(`Family member ${name} is ${age} years old`);
+// }
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+//
+/*
+Coding Challenge #2
+Let's continue with our football betting app! Keep using the 'game' variable from
+before.
+Your tasks:
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them
+(except for "draw"). Hint: Note how the odds and the game objects have the
+same property names 😉
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this:
+{
+Gnarby: 1,
+Hummels: 1,
+Lewandowski: 2
 }
+GOOD LUCK 😀
+*/
 
-// Methods
-//Here we can check if a method exists before calling it
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist'); // Focaccia, Pasta
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist'); // Method does not exist
-
-// Arrays
-let currentYear = new Date().getFullYear();
-console.log(currentYear);
-const family = [
-    { name: 'Eric', age: currentYear - 1980 },
-    { name: 'Rebecca', age: currentYear - 1983 },
-    { name: 'Joseph', age: currentYear - 2004 },
-    { name: 'Edward', age: currentYear - 2007 },
-    { name: 'Zachary', age: currentYear - 2009 },
-];
-console.log(family);
-console.log(family[0]?.name) ?? 'Array Empty'; //Eric
-console.log(family[0]?.age) ?? 'Array Empty'; //41
-
-// END OPTIONAL CHAINING (?)
-////////////////////////////////////////////////
-// START LOOPING OBJECTS: OBJECT KEYS, VALUES, and ENTRIES
+/*
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+*/
