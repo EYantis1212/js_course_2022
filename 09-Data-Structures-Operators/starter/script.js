34464ff1a843b5e60f589d65d32891c36fffd8a3
Yantis <yantis1212@outlook.com>
2021-12-26 16:57:11 -0700

    ENDS MAPS / START CODING CHALLENGE #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -853,3 +853,77 @@ Get the team names directly from the game object, don't hardcode them
 // END SETS
 ////////////////////////////////////////////////
 // START MAPS
+
+// IN MAPS...KEYS CAN BE ANY DATA TYPE EVEN FUNCTION!!!!!!!!!!!
+
+// const rest = new Map();
+
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy'); //These not only set...but also return!
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+// rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//     .set('open', 11)
+//     .set('close', 23)
+//     .set(true, 'We are open :D')
+//     .set(false, 'We are closed :(');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('open'));
+// console.log(rest.has(11));
+
+// rest.delete(2);
+// console.log(rest.size);
+
+// // rest.clear();
+
+// // This wont work because the arrays are different
+// rest.set([1, 2], 'Test');
+// rest.get([1, 2]);
+
+// // Here it will work
+// const arr = [1, 2, 3];
+// rest.set(arr, 'Test');
+// console.log(rest.get(arr));
+
+// rest.set(document.querySelector('h1'), 'Heading');
+
+// MAPS: ITERATION ////////////////////////////////
+
+const question = new Map([
+    ['question', 'What is the best programming language?'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'JavaScript'],
+    ['correct', 3],
+    [true, 'You are Correct!'],
+    [false, 'You are Wrong!'],
+]);
+
+// Convert Object to map
+function getByValue(map, search) {
+    for (let [key, value] of question.entries()) {
+        if (value === search) {
+            return key;
+        }
+    }
+}
+const hoursMap = new Map(Object.entries(restaurant.openingHours));
+console.log(hoursMap);
+console.log(question.get('question'));
+for (const [key, value] of question) {
+    if (typeof key === 'number') console.log(key, value);
+}
+// const answer = prompt('Your Answer');
+
+// console.log(
+//     question.get(question.get('correct') === getByValue(question, answer))
+// );
+
+// Convert Map to Array
+console.log([...question]);
