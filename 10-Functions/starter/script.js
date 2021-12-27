d4cd8d361a8af991a7880e9bb2cc3b805d6c363f
Yantis <yantis1212@outlook.com>
2021-12-27 00:06:32 -0700

    START PASSING ARGUMENTS

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -3,3 +3,24 @@
 !START SECTION 10 -------------------------------------------------------
 */
 //!Start Default Parameters
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 3);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 3); // This is how you can skip a parameter
+
+//!Start Passing Arguments
