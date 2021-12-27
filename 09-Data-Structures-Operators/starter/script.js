7da8a3866c6623aefe5aeb757684d881798f66c3
Yantis <yantis1212@outlook.com>
2021-12-26 17:25:38 -0700

    END CODING CHALLENGE 3 / START WORKING WITH STRINGS

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -895,30 +895,30 @@ Get the team names directly from the game object, don't hardcode them
 
 // MAPS: ITERATION ////////////////////////////////
 
-const question = new Map([
-    ['question', 'What is the best programming language?'],
-    [1, 'C'],
-    [2, 'Java'],
-    [3, 'JavaScript'],
-    ['correct', 3],
-    [true, 'You are Correct!'],
-    [false, 'You are Wrong!'],
-]);
+// const question = new Map([
+//     ['question', 'What is the best programming language?'],
+//     [1, 'C'],
+//     [2, 'Java'],
+//     [3, 'JavaScript'],
+//     ['correct', 3],
+//     [true, 'You are Correct!'],
+//     [false, 'You are Wrong!'],
+// ]);
 
-// Convert Object to map
-function getByValue(map, search) {
-    for (let [key, value] of question.entries()) {
-        if (value === search) {
-            return key;
-        }
-    }
-}
-const hoursMap = new Map(Object.entries(restaurant.openingHours));
-console.log(hoursMap);
-console.log(question.get('question'));
-for (const [key, value] of question) {
-    if (typeof key === 'number') console.log(key, value);
-}
+// // Convert Object to map
+// function getByValue(map, search) {
+//     for (let [key, value] of question.entries()) {
+//         if (value === search) {
+//             return key;
+//         }
+//     }
+// }
+// const hoursMap = new Map(Object.entries(restaurant.openingHours));
+// console.log(hoursMap);
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//     if (typeof key === 'number') console.log(key, value);
+// }
 // const answer = prompt('Your Answer');
 
 // console.log(
@@ -926,4 +926,58 @@ for (const [key, value] of question) {
 // );
 
 // Convert Map to Array
-console.log([...question]);
+// console.log([...question]);
+
+// END MAPS
+////////////////////////////////////////////////
+// START DATA STRUCTURES, MODERN OPERATORS AND STRINGS
+/*
+Coding Challenge #3
+Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+⚽
+[FIRST HALF] 17:
+GOAL
+GOOD LUCK 😀
+*/
+// const gameEvents = new Map([
+//     [17, '⚽ GOAL'],
+//     [36, '🔁 Substitution'],
+//     [47, '⚽ GOAL'],
+//     [61, '🔁 Substitution'],
+//     [64, '🔶 Yellow card'],
+//     [69, '🔴 Red card'],
+//     [70, '🔁 Substitution'],
+//     [72, '🔁 Substitution'],
+//     [76, '⚽ GOAL'],
+//     [80, '⚽ GOAL'],
+//     [92, '🔶 Yellow card'],
+// ]);
+
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
+// gameEvents.delete(64);
+// console.log(gameEvents);
+// console.log(`An event happened, on
+// average, every ${90 / gameEvents.size} minutes`);
+
+// for (const [key, value] of gameEvents) {
+//     if (key <= 45) {
+//         console.log(`[Fisrt Half]${key}: ${value}`);
+//     } else console.log(`[Second Half]${key}: ${value}`);
+// }
+
+// END CODING CHALLENGE 3
+////////////////////////////////////////////////
+// START WORKING WITH STRINGS
