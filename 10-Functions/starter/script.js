71d05d67c40974c2f08fa25c2423c3d2126e6d41
Yantis <yantis1212@outlook.com>
2021-12-27 15:24:35 -0700

    Start Functions Accepting Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -24,3 +24,27 @@ createBooking('LH123', 5);
 createBooking('LH123', undefined, 3); // This is how you can skip a parameter
 
 //!Start Passing Arguments
+
+const flight = 'LH234';
+const eric = {
+  name: 'Eric Yantis',
+  passport: 12355162634,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 12355162634) {
+    console.log('Check in');
+  } else console.log('Wrong Passport');
+};
+console.log(checkIn(flight, eric));
+console.log(flight, eric); // Flight is the same but name was changed
+// This is because the flight was a primitive so it was a copy...
+//The name was an object...so it was a copy but just of the reference..
+// The object passed in was a reference to the original so it can change it
+
+//Summary - variables contain either a primitive data type or a reference to a data structure
+//Passing in a variable will make a copy of the primitive or data reference.
+
+//! Functions Accepting Callback Functions
