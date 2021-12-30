18ae748f71fc734dec3262001b91934263192300
Yantis <yantis1212@outlook.com>
2021-12-30 08:24:14 -0700

    Internationalizing Numbers (Intl)

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -18,17 +18,17 @@ const account1 = {
   loanAmount: 0,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2020-11-18T21:31:17.178Z',
+    '2020-12-23T07:42:02.383Z',
+    '2021-01-28T09:15:04.904Z',
+    '2021-04-01T10:17:24.185Z',
+    '2021-12-25T14:11:59.604Z',
+    '2021-12-27T17:01:17.194Z',
+    '2021-12-29T23:36:17.929Z',
+    '2021-12-30T10:51:36.790Z',
   ],
-  currency: 'EUR',
-  locale: 'pt-PT', // de-DE
+  currency: 'USD',
+  locale: 'en-US', // de-DE
 };
 
 const account2 = {
@@ -84,10 +84,25 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions
+const formatMovementDate = function (date, locale) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  if (daysPassed === 1) return 'Yesterday';
+  else {
+    return new Intl.DateTimeFormat(locale).format(date);
+  }
+};
+
+// Display Movements
 const displayMovements = function (account, sort = false) {
   containerMovements.innerHTML = '';
-
   const movs = sort
     ? account.movements.slice().sort((a, b) => a - b)
     : account.movements;
@@ -95,10 +110,10 @@ const displayMovements = function (account, sort = false) {
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const date = new Date(account.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, '0');
-    const month = `${date.getMonth() + 1}`.padStart(2, '0');
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date, account.locale);
+
+    //
+
     const html = `
 	  <div class="movements__row">
 		<div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
@@ -186,12 +201,19 @@ btnLogin.addEventListener('click', function (e) {
     containerApp.style.opacity = 100;
     // Create Current Date
     const now = new Date();
-    const day = `${now.getDate()}`.padStart(2, '0');
-    const month = `${now.getMonth() + 1}`.padStart(2, '0');
-    const year = now.getFullYear();
-    const hour = now.getHours().padStart(2, '0');
-    const minutes = `${now.getMinutes()}`.padStart(2, 0);
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${minutes}`;
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      weekday: 'long',
+    };
+    const locale = navigator.language;
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
@@ -280,112 +302,112 @@ btnSort.addEventListener('click', function (e) {
 
 //! Converting and Checking Numbers
 
-console.log(23 === 23.0); //true...all are floating point numbers
-console.log(0.1 + 0.2); //0.30000000000000004
-// This is why you can not do precise scientific or financial calculations in JS
-console.log(0.1 + 0.2 === 0.3); //False
+// console.log(23 === 23.0); //true...all are floating point numbers
+// console.log(0.1 + 0.2); //0.30000000000000004
+// // This is why you can not do precise scientific or financial calculations in JS
+// console.log(0.1 + 0.2 === 0.3); //False
 
-// Convert to number
-console.log(Number('23'));
-console.log(+'23');
+// // Convert to number
+// console.log(Number('23'));
+// console.log(+'23');
 
-// Parsing
-console.log(Number.parseInt('94px', 10)); //94  ... the 10 means base 10... use 2 for binary
-console.log(Number.parseInt('2.5rem')); //2
-console.log(Number.parseFloat('2.5rem')); // 2.5
+// // Parsing
+// console.log(Number.parseInt('94px', 10)); //94  ... the 10 means base 10... use 2 for binary
+// console.log(Number.parseInt('2.5rem')); //2
+// console.log(Number.parseFloat('2.5rem')); // 2.5
 
-// is NaN
-console.log(Number.isNaN(20)); //true
-console.log(Number.isNaN('20')); // true
-console.log(Number.isNaN(+'ff')); // true
-console.log(Number.isNaN(23 / 0)); // false
+// // is NaN
+// console.log(Number.isNaN(20)); //true
+// console.log(Number.isNaN('20')); // true
+// console.log(Number.isNaN(+'ff')); // true
+// console.log(Number.isNaN(23 / 0)); // false
 
-// Best way to check if number
-console.log(Number.isFinite(20)); //true
-console.log(Number.isFinite('20')); //false
-console.log(Number.isFinite(+'20xf')); // false
-console.log(Number.isFinite(23 / 0)); // false
+// // Best way to check if number
+// console.log(Number.isFinite(20)); //true
+// console.log(Number.isFinite('20')); //false
+// console.log(Number.isFinite(+'20xf')); // false
+// console.log(Number.isFinite(23 / 0)); // false
 
-//! Math and Rounding
+// //! Math and Rounding
 
-console.log(Math.sqrt(25)); // 5
-console.log(25 ** (1 / 2)); // 5
-console.log(8 ** (1 / 3)); //2 cuberoot
+// console.log(Math.sqrt(25)); // 5
+// console.log(25 ** (1 / 2)); // 5
+// console.log(8 ** (1 / 3)); //2 cuberoot
 
-console.log(Math.max(5, 23, '67', 84, 24, 23)); //84
-console.log(Math.min(5, 23, '67', 84, 24, 23)); //5
+// console.log(Math.max(5, 23, '67', 84, 24, 23)); //84
+// console.log(Math.min(5, 23, '67', 84, 24, 23)); //5
 
-console.log(Math.PI); //3.141592653589793
+// console.log(Math.PI); //3.141592653589793
 
-console.log(Math.PI * Number.parseFloat('10px') ** 2); // Area of circle with 10px radius
+// console.log(Math.PI * Number.parseFloat('10px') ** 2); // Area of circle with 10px radius
 
-// Random
+// // Random
 
-console.log(Math.trunc(Math.random() * 6) + 1);
+// console.log(Math.trunc(Math.random() * 6) + 1);
 
-const randomInt = (min, max) =>
-  Math.trunc(Math.random() * (max - min + 1)) + min;
-console.log(randomInt(10, 20));
+// const randomInt = (min, max) =>
+//   Math.trunc(Math.random() * (max - min + 1)) + min;
+// console.log(randomInt(10, 20));
 
-//Rounding integers
+// //Rounding integers
 
-console.log(Math.trunc(23.6)); // 23 Takes decimal away only
+// console.log(Math.trunc(23.6)); // 23 Takes decimal away only
 
-console.log(Math.round(23.3)); //23
-console.log(Math.round(23.6)); //24
+// console.log(Math.round(23.3)); //23
+// console.log(Math.round(23.6)); //24
 
-console.log(Math.ceil(23.3)); //24
-console.log(Math.ceil(23.6)); //24
+// console.log(Math.ceil(23.3)); //24
+// console.log(Math.ceil(23.6)); //24
 
-console.log(Math.floor(23.6)); //23
-console.log(Math.floor(23.3)); //23
+// console.log(Math.floor(23.6)); //23
+// console.log(Math.floor(23.3)); //23
 
-// Rounding Decimals
-console.log((2.7).toFixed(0)); // String of 3
-console.log((2.2).toFixed(0)); // String of 2
+// // Rounding Decimals
+// console.log((2.7).toFixed(0)); // String of 3
+// console.log((2.2).toFixed(0)); // String of 2
 
-console.log((2.2).toFixed(1)); // String of 2.2
-console.log((2.7).toFixed(1)); // String of 2.7
+// console.log((2.2).toFixed(1)); // String of 2.2
+// console.log((2.7).toFixed(1)); // String of 2.7
 
-console.log((2.1234556).toFixed(3)); // String of 2.123
-console.log(+(2.734567257).toFixed(3)); // Number of 2.735
+// console.log((2.1234556).toFixed(3)); // String of 2.123
+// console.log(+(2.734567257).toFixed(3)); // Number of 2.735
 
-//! The Remainder Operator
+// //! The Remainder Operator
 
-console.log(5 % 2); //1
+// console.log(5 % 2); //1
 
-const isEven = n => n % 2 === 0;
+// const isEven = n => n % 2 === 0;
 
-//! Numeric Separators
+// //! Numeric Separators
 
-const diameter = 287_460_000_000;
-console.log(diameter);
+// const diameter = 287_460_000_000;
+// console.log(diameter);
 
-const priceCents = 34_99;
+// const priceCents = 34_99;
 
-const transferFee = 15_00;
-const transferFee2 = 1_500;
+// const transferFee = 15_00;
+// const transferFee2 = 1_500;
 
-//! Working with BigInt
+// //! Working with BigInt
 
-//JavaScript Numbers are stored in 64bits..53 bits or 1s and 0s for numbers and rest for signs and decimals
+// //JavaScript Numbers are stored in 64bits..53 bits or 1s and 0s for numbers and rest for signs and decimals
 
-const biggestNum = Number.MAX_SAFE_INTEGER;
-console.log(biggestNum); //9007199254740991
-// Things get weird after this number
+// const biggestNum = Number.MAX_SAFE_INTEGER;
+// console.log(biggestNum); //9007199254740991
+// // Things get weird after this number
 
-console.log(34053456236234562345623462346234623n); // Now this is a bigInt
-console.log(BigInt(239429354567458458));
+// console.log(34053456236234562345623462346234623n); // Now this is a bigInt
+// console.log(BigInt(239429354567458458));
 
-//operations
-console.log(1000000n * 2340234056n);
+// //operations
+// console.log(1000000n * 2340234056n);
 
-// You just can not mix BigInts and other types...because they are stored in seperate places
+// // You just can not mix BigInts and other types...because they are stored in seperate places
 
-// You can compare between though like:
-console.log(20n > 19); //true
+// // You can compare between though like:
+// console.log(20n > 19); //true
 
-//! Creating Dates
+// //! Creating Dates
 
 // Create a date
 
@@ -403,25 +425,39 @@ console.log(20n > 19); //true
 
 // Working with date methods
 
-const future = new Date(2037, 10, 19, 15, 23);
-console.log(future); //Thu Nov 19 2037 15:23:00 GMT-0700 (Mountain Standard Time)
-console.log(future.getFullYear()); //2037
-console.log(future.getMonth()); // 10  ---This is November because 0 based
-console.log(future.getDate()); // 19 <---day
-console.log(future.getHours()); // 15
-console.log(future.getMinutes()); // 23
-console.log(future.getTime()); //2142282180000   <--- timestamp
-console.log(future.getTimezoneOffset()); // 420
-console.log(future.toISOString()); //2037-11-19T22:23:00.000Z
-console.log(future.toDateString()); //Thu Nov 19 2037
-console.log(future.getTime()); // 2142282180000 milliseconds since jan 1 1970
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(future); //Thu Nov 19 2037 15:23:00 GMT-0700 (Mountain Standard Time)
+// console.log(future.getFullYear()); //2037
+// console.log(future.getMonth()); // 10  ---This is November because 0 based
+// console.log(future.getDate()); // 19 <---day
+// console.log(future.getHours()); // 15
+// console.log(future.getMinutes()); // 23
+// console.log(future.getTime()); //2142282180000   <--- timestamp
+// console.log(future.getTimezoneOffset()); // 420
+// console.log(future.toISOString()); //2037-11-19T22:23:00.000Z
+// console.log(future.toDateString()); //Thu Nov 19 2037
+// console.log(future.getTime()); // 2142282180000 milliseconds since jan 1 1970
 
-console.log(new Date(2142282180000)); //Thu Nov 19 2037 15:23:00 GMT-0700 (Mountain Standard Time)
-console.log(Date.now()); //1640871640326
+// console.log(new Date(2142282180000)); //Thu Nov 19 2037 15:23:00 GMT-0700 (Mountain Standard Time)
+// console.log(Date.now()); //1640871640326
 
-future.setFullYear(2021);
-console.log(future); //Fri Nov 19 2021
+// future.setFullYear(2021);
+// console.log(future); //Fri Nov 19 2021
 
 //! Adding Dates to Bankist App (see app)
 
 //! Operations With Dates
+
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(+future); //2142282180000
+
+// const calcDaysPassed = (date1, date2) =>
+//   Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+// const days1 = calcDaysPassed(new Date(2037, 10, 19), new Date(2037, 11, 19));
+// console.log(days1); // 30
+
+//! Internationalizing Dates (Intl)
+// New API to Internationalize things in JavaScript
+
+//! Internationalizing Numbers (Intl)
