68b432572c99c9ccd4e9644585bab1868e327b34
Yantis <yantis1212@outlook.com>
2022-01-02 12:44:01 -0700

    OOP in JavaScript

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,3 +1,35 @@
 'use strict';
 
 //! What is Object-Oriented Programming
+
+// We use objects to model real world or abstract features
+
+// Objects may contain data (Properties) and code (Methods)
+
+// By using objects, we pack data and corresponding behavior into one block
+
+// OOP objects are self-contained pieces/blocks of code
+// They are used as building blocks for applications and interact with one another
+// These interactions happen through an API: methods that the code outside of the object can access and use to communicate with the object
+
+// A Class is like a blueprint from which we create new objects
+
+//! The Four Principles of OOP
+
+//! 1. Abstraction
+/* Ignoring or Hiding details that don't matter, allowing for an overview perspective of what we are
+implementing, instead of messing with details that do not matter.
+
+//! 2. Encapsulation
+/* Keep certain properties and methods private inside the class, so they are not accessible outside it.
+Some Methods can be exposed as a public interface (API)
+
+//! 3. Inheritance
+/* Makes all properties and methods of a certain class available to a child class.
+This forms a hierarchal relationship between classes.  This allows us to reuse common logic and
+to model real-world relationships.
+
+//! 4. Polymorphism
+/* A child class can overwrite a method it inherited from a parent class. */
+
+//! OOP in JavaScript
