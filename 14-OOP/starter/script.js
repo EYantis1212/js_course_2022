42ba50198daf05702ea1a6f2a037d81f5caf7da3
Yantis <yantis1212@outlook.com>
2022-01-02 18:13:10 -0700

    Coding Challenge 1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -51,3 +51,33 @@ console.log(eric); //Person {firstName: 'Eric', birthYear: 1980}
 // 4. functions automatically returns {}
 
 //! Prototypes
+Person.prototype.calcAge = function () {
+	console.log(2022 - this.birthYear);
+};
+
+eric.calcAge(); // 42
+
+console.log(Person.prototype.isPrototypeOf(eric)); // true
+// Person.prototype is the prototype that the object 'eric' is pointing to with its __proto__
+
+Person.prototype.species = 'Human';
+
+console.log(eric.hasOwnProperty('firstName')); // true
+console.log(eric.hasOwnProperty('species')); // false
+
+//! Coding Challenge #1
+// Your tasks:
+// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+// 'speed' property. The 'speed' property is the current speed of the car in
+// km/h
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10,
+// and log the new speed to the console
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+// the new speed to the console
+// 4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+// 'brake' multiple times on each of them
+// Test data:
+// §
+// §
+// Data car 1: 'BMW' going at 120 km/h
+// Data car 2: 'Mercedes' going at 95km/hr
