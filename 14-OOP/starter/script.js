e65fbe64236f85855a0c52923fc0db1e40a3ea2d
Yantis <yantis1212@outlook.com>
2022-01-02 20:03:21 -0700

    Inheritance Between Classes:ES6 Classes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -182,18 +182,64 @@ Person.hey(); // Hello!!!
 PersonCl.hey();
 
 //! Object.crate
-const PersonProto = {
-	calcAge() {
-		console.log(2022 - this.birthYear);
-	},
-	init(firstName, birthYear) {
-		this.firstName = firstName;
-		this.birthYear = birthYear;
-	},
-};
-const joseph = Object.create(PersonProto);
-joseph.init('Joseph', 2004);
-console.log(joseph);
-joseph.calcAge();
+// const PersonProto = {
+// 	calcAge() {
+// 		console.log(2022 - this.birthYear);
+// 	},
+// 	init(firstName, birthYear) {
+// 		this.firstName = firstName;
+// 		this.birthYear = birthYear;
+// 	},
+// };
+// const joseph = Object.create(PersonProto);
+// joseph.init('Joseph', 2004);
+// console.log(joseph);
+// joseph.calcAge();
 
 //! Coding Challenge #2
+/*
+Coding Challenge #2
+Your tasks:
+1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide
+by 1.6)
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+converts it to km/h before storing the value, by multiplying the input by 1.6)
+4. Create a new car and experiment with the 'accelerate' and 'brake'
+methods, and with the getter and setter.
+Test data:
+§
+Data car 1: 'Ford' going at 120 km/h
+GOOD LUCK 😀
+*/
+class CarCl {
+	constructor(make, speed) {
+		this.make = make;
+		this.speed = speed;
+	}
+	accelerate() {
+		this.speed += 10;
+		console.log(`${this.make} is now going ${this.speed}km/h`);
+	}
+	brake() {
+		this.speed - +10;
+		console.log(`${this.make} is now going ${this.speed}km/h`);
+	}
+	get speedUS() {
+		return this.speed / 1.6;
+	}
+	set speedUS(speed) {
+		this.speed = speed * 1.6;
+	}
+}
+const myCar = new CarCl('Nissan', 100);
+console.log(myCar);
+console.log(myCar.speedUS);
+myCar.speedUS = 150;
+console.log(myCar);
+console.log(myCar.speedUS);
+myCar.accelerate();
+myCar.accelerate();
+console.log(myCar.speedUS);
+
+//! Inheritance Between "Classes":ES6 Classes
