da3b46bc5b22ce62cb1d743bd10df852325d236b
Yantis <yantis1212@outlook.com>
2022-01-24 02:17:32 -0700

    practiced importing and exporting modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,1 +1,17 @@
 // START SECTION 17
+// Importing Module
+// import { addToCart, totalPrice as tp, totalQuantity } from './shoppingCart.js';
+console.log('Importing Module');
+// console.log(totalPrice, totalQuantity);
+// addToCart('bread, 5');
+
+// import * as ShoppingCart from './shoppingCart.js';
+// console.log(ShoppingCart);
+
+// const addCartItem = ShoppingCart.addToCart;
+
+// addCartItem('doll', 35);
+
+import add from './shoppingCart.js';
+
+add('game', 25);
