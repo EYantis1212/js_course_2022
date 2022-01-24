61c6f80fad41347551fbcc34a887374aa7e4ccc8
Yantis <yantis1212@outlook.com>
2022-01-23 18:47:29 -0700

    created function to run promises in parallel using Promise all combinator

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -389,3 +389,30 @@ const whereAmI = async function (country) {
     console.error(`${err.message}`);
   }
 })();
+
+// Running Promises in Parallel
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`Country not found (${response.status})`);
+    }
+    return response.json();
+  });
+};
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('usa', 'japan', 'canada');
